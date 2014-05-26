714f272ad475933fbfc0f683a76fb14714d947f6
Louis Pinot <lpvillechenon@gmail.com>
2014-05-26 16:31:46 +0200

    basic crud methods implemented for soldiers

--- app/controllers/soldiers_controller.rb
+++ app/controllers/soldiers_controller.rb
@@ -1,5 +1,9 @@
 class SoldiersController < ApplicationController
+  before_action :find_soldier, only: [:show, :edit, :update, :destroy]
+  before_action :attributes, only: [:update, :create]
+
   def index
+    @soldiers = Soldier.all
   end
 
   def show
@@ -9,14 +13,27 @@ class SoldiersController < ApplicationController
   end
 
   def update
+    @soldier(attributes)
   end
 
   def new
   end
 
   def create
+    Soldier.create(attributes)
   end
 
   def destroy
+    @soldier.destroy
   end
+
+  private
+
+  def find_soldier
+    @soldier = Soldier.find(params[:id])
+  end
+
+  def attributes
+    attributes = params.require(:soldier).permit(:first_name, :last_name, :service_number, :rank, :unit, :date_of_death, :place_of_death, :cemetery, :source, :bio)
 end
+
