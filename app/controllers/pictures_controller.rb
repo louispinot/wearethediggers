040e97a598cbe47083cdd3a13854524321622483
Louis Pinot <lpvillechenon@gmail.com>
2014-05-27 18:04:26 +0200

    customized homepage and registration pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,8 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only: [:show, :edit, :update, :destroy]
+  before_action :attributes, only: [:update, :create]
+
+
   def index
     @pictures = Picture.all
   end
@@ -20,4 +24,15 @@ class PicturesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def find_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  def attributes
+    attributes = params.require(:picture).permit(:picture_url, :ref, :author, :place, :copyrights_owner, :description, :date)
+  end
+
 end
