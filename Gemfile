d2fad2c2764a58cbebb524537d2cc4014ec783bb
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-05-30 14:11:29 +0200

    will paginate working but no shuffle

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ gem 'figaro', '1.0.0.rc1'
 gem "pundit"
 
 gem 'medium-editor-rails', '0.9.3'
+gem 'will_paginate', '~> 3.0'
+
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,7 +4,7 @@ class PicturesController < ApplicationController
 
 
   def index
-    @pictures = Picture.all.shuffle
+    @pictures = Picture.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
